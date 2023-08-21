25911603fdc5cd399b7e8123603f83c5ad3b47f8
Yana Trushnikova <58902064+richpeach-bot@users.noreply.github.com>
2023-08-22 01:59:11 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/arrow-next.svg" alt="next arrow"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/arrow-prev.svg" alt="prev arrow"></button>'
+    });
+});
